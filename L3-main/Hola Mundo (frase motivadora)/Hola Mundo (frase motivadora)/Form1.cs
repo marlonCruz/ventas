088009fc74fb4f902fe9ed7474416cda38a4d7ea
marlonCruz <78571332+marlonCruz@users.noreply.github.com>
2021-06-07 20:57:09 -0600

    Add files via upload

---
+++ L3-main/Hola Mundo (frase motivadora)/Hola Mundo (frase motivadora)/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hola_Mundo_frase_motivadora_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("No te preocupes por lo mas que lo estes pasando ahora, La vida es una montaña rusa y despues de las bajadas, vienen las subidas");
+       }
+    }
+}
